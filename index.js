ac87ae3397e4de1f21b24d8b5cbe507b41246fc5
gulshan-ara <gulshan3015@gmail.com>
2024-10-15 23:26:40 +0600

    trying to make the api work

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+// Middleware to parse JSON request bodies (if you need it in other parts of the app)
+app.use(express.json());
+
+// Home route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-app.post("/create-checkout-session", async (req, res) => {
-  const products = req.body;
-
-  const lineItems = products.map((product) => ({
-    price_data: {
-      currency: "inr",
-      product_data: {
-        name: product.card?.info?.name,
-      },
-      unit_amount:
-        product.card?.info?.price || product.card?.info?.defaultPrice,
-    },
-    quantity: product.quantity,
-  }));
-
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: lineItems,
-    mode: "payment",
-    successUrl: "",
-    cancelUrl: "",
-  });
-
-  res.json({ id: session.id });
-
-  res.send({
-    message: "New user was added to the list",
-  });
+  console.log(`App listening at http://localhost:${port}`);
 });
